2ad3c454a6cf4425dffa16d6c8c80ccaadbe974b
enitsed <sllki1@naver.com>
2017-11-01 19:57:41 +0900

    etc

--- src/majestyUtility/MEmulator.java
+++ src/majestyUtility/MEmulator.java
@@ -1,25 +1,23 @@
 package majestyUtility;
 
 import java.awt.Color;
-import java.awt.Font;
+import java.io.File;
 
 import javax.swing.JFrame;
 
 public class MEmulator extends JFrame {
 	int width = 1024;
 	int height = 1200;
-	
-	
+
 	public MEmulator() {
-		File 
-		
+		File jpg = new File("//resources//0000.bmp");
+
 		setBackground(Color.BLACK);
 		setTitle("Majesty");
 		setSize(width, height);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-
 
 	public static void main(String[] args) {
 		new MEmulator();
